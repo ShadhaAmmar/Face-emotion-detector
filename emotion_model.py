c27f4c0bd26b286fa593f013b989a5b9ab689f2f
Shadha AMMAR <shadhaammar@users.noreply.github.com>
2026-06-15 01:36:30 +0100

    feat: add compound/complex emotions support

--- emotion_model.py
+++ emotion_model.py
@@ -18,7 +18,44 @@ class EmotionModel:
     def predict(self, face_img):
         processed = self.preprocess(face_img)
         preds = self.model.predict(processed, verbose=0)[0]
-        emotion_idx = np.argmax(preds)
-        emotion = self.emotion_labels[emotion_idx]
-        prob = preds[emotion_idx]
-        return emotion, prob
+        
+        # get indices sorted by probability descending
+        sorted_indices = np.argsort(preds)[::-1]
+        top1_idx = sorted_indices[0]
+        top2_idx = sorted_indices[1]
+        
+        top1_label = self.emotion_labels[top1_idx]
+        top2_label = self.emotion_labels[top2_idx]
+        
+        top1_prob = preds[top1_idx]
+        top2_prob = preds[top2_idx]
+        
+        # define complex emotions when top two classes have high probabilities
+        if top1_prob > 0.35 and top2_prob > 0.20:
+            pair = {top1_label, top2_label}
+            if pair == {"Happy", "Surprised"}:
+                return "Happily Surprised", top1_prob
+            elif pair == {"Fearful", "Surprised"}:
+                return "Awestruck", top1_prob
+            elif pair == {"Angry", "Surprised"}:
+                return "Outraged", top1_prob
+            elif pair == {"Sad", "Surprised"}:
+                return "Disappointed", top1_prob
+            elif pair == {"Angry", "Disgusted"}:
+                return "Hostile", top1_prob
+            elif pair == {"Angry", "Sad"}:
+                return "Frustrated", top1_prob
+            elif pair == {"Fearful", "Sad"}:
+                return "Anxious", top1_prob
+            elif pair == {"Disgusted", "Sad"}:
+                return "Remorseful", top1_prob
+            elif pair == {"Happy", "Disgusted"}:
+                return "Cynical", top1_prob
+            elif pair == {"Neutral", "Sad"}:
+                return "Melancholic", top1_prob
+            elif pair == {"Neutral", "Happy"}:
+                return "Content", top1_prob
+            elif pair == {"Neutral", "Angry"}:
+                return "Stern", top1_prob
+                
+        return top1_label, top1_prob
